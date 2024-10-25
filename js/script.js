0394ac571ba33dda0851789d60fa0c7b04904ad0
ClaRa-git <rameau.claire.cr@gmail.com>
2024-10-25 15:40:28 +0200

    Ajout des fichiers + completion.txt au projet

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Modèle de données produits
+const products = [
+    {
+        name: 'Tartiflette à réaction',
+        price: 599.99,
+        stock: 26
+    },
+    {
+        name: 'Hélicoptère à pédales',
+        price: 250.95,
+        stock: 12
+    },
+    {
+        name: 'Géranium électrique',
+        price: 398.57,
+        stock: 178
+    }
+];
+
+// DOMContentLoaded
+document.addEventListener( 'DOMContentLoaded', function() {
+    console.log( '%c🚀', 'font-size:180px' );
+    // Votre code ici !
+});
