8ff6657a5fb992ce1873ab88c8b9e41e94bdb635
ClaRa-git <rameau.claire.cr@gmail.com>
2024-10-28 18:07:46 +0100

    Suppresion du listenerEvent sur les th et ajout d'une ligne à la place. L'icone de suppression totale a été mise dans la colonne Supprimer

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ const products = [
 ];
 
 // Fonction pour remplir le localStorage avec products pour les tests
+// !!! Efface tous les produits présents dans le localStorage !!!
 function fillLocalStorage() {
     products.forEach((product) => {
         product.name = product.name.toUpperCase();
@@ -25,6 +26,8 @@ function fillLocalStorage() {
     localStorage.setItem('listeProduits', JSON.stringify(products));
 }
 //fillLocalStorage();
+
+
 
 // DOMContentLoaded
 document.addEventListener('DOMContentLoaded', function () {
@@ -45,71 +48,25 @@ document.addEventListener('DOMContentLoaded', function () {
         return [...tableau].sort((a, b) => { return a.stock - b.stock; });
     }
 
-    //Fonction pour vérifier si le stock est vide ou non et affichier l'alerte
+    //Fonction pour vérifier si le stock est vide ou non et affichier l'alerte ainsi que la ligne de tri
     function alerteVide() {
         let listeProduits = JSON.parse(localStorage.getItem("listeProduits")) || []; // Récupération des produits, si vide alors []
 
         const divAlert = document.getElementById("alert");
+        const ligneTri = document.getElementById("tri");
 
         if (listeProduits.length <= 0) {
             divAlert.style.display = "block"; // Si le stock est vide, on affiche l'alerte
+            ligneTri.style.display = "none"; // On cache la ligne de tri
         } else {
             divAlert.style.display = "none"; // Sinon on cache l'alerte
+            ligneTri.style.display = "table-row"; // On affiche la ligne de tri
         }
     }
 
     // Fonction pour afficher les produits dans le tableau
     function displayProducts() {
         let listeProduits = JSON.parse(localStorage.getItem("listeProduits")) || []; // Récupération des produits, si vide alors []
-
-        // On récupère les 3 th du tableau
-        const thName = document.getElementById("productName");
-        const thPrice = document.getElementById("productPrice");
-        const thQuantity = document.getElementById("productQuantity");
-        const thDeleteAll = document.getElementById("productsDelete");
-
-        thName.title = "Cliquez pour trier par nom";
-        thPrice.title = "Cliquez pour trier par prix";
-        thQuantity.title = "Cliquez pour trier par quantité";
-        thDeleteAll.title = "Cliquez pour supprimer tous les produits";
-
-        // On est à l'écoute du clic sur le nom pour trier par nom
-        thName.addEventListener('click', () => {
-            if (listeProduits.length > 1) { // On tri uniquement si on a plus d'un produit
-                localStorage.setItem('listeProduits', JSON.stringify(sortByName(listeProduits)));
-                listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
-                displayProducts();
-            }
-        });
-
-        // On est à l'écoute du clic sur le prix pour trier par prix
-        thPrice.addEventListener('click', () => {
-            if (listeProduits.length > 1) { // On tri uniquement si on a plus d'un produit
-                localStorage.setItem('listeProduits', JSON.stringify(sortByPrice(listeProduits)));
-                listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
-                displayProducts();
-            }
-        });
-
-        // On est à l'écoute du clic sur la quantité pour trier par quantité
-        thQuantity.addEventListener('click', () => {
-            if (listeProduits.length > 1) { // On tri uniquement si on a plus d'un produit
-                localStorage.setItem('listeProduits', JSON.stringify(sortByQuantity(listeProduits)));
-                listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
-                displayProducts();
-            }
-        });
-
-        // On est à l'écoute du clic sur le bouton pour supprimer tous les produits
-        thDeleteAll.addEventListener('click', () => {
-            if (window.confirm("Voulez-vous supprimer la totalité des produits du stock ?")) {
-                //console.log("Suppression totale");
-                listeProduits = [];
-                localStorage.setItem("listeProduits", JSON.stringify(listeProduits));
-                alerteVide();
-                displayProducts();
-            }
-        });
 
         const bodyTableau = document.getElementById("products");
         bodyTableau.innerHTML = ""; // On vide le tableau
@@ -141,10 +98,12 @@ document.addEventListener('DOMContentLoaded', function () {
                 tableRow.classList.add("table-success");
             }
 
-            const tdButtonAddMinus = document.createElement("td"); // Création de la cellule pour les boutons +/-
+            // Création de la cellule pour les boutons +/-
+            const tdButtonAddMinus = document.createElement("td");
             tdButtonAddMinus.className = "text-nowrap";
 
-            const buttonMinus = document.createElement("button"); // Création du bouton de retrait
+            // Création du bouton de retrait
+            const buttonMinus = document.createElement("button");
             buttonMinus.className = "btn btn-primary btn-sm stock-del";
             buttonMinus.innerHTML = "&minus;";
             // On est à l'écoute du clic sur le bouton pour retirer -1 au nombre en stock
@@ -159,7 +118,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
             });
 
-            const buttonAdd = document.createElement("button"); // Création du bouton d'ajout
+            // Création du bouton d'ajout
+            const buttonAdd = document.createElement("button");
             buttonAdd.className = "btn btn-outline-primary btn-sm stock-add";
             buttonAdd.innerHTML = "&plus;";
             // On est à l'écoute du clic sur le bouton pour ajouter +1 au nombre en stock
@@ -173,7 +133,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
             tdButtonAddMinus.append(buttonMinus, buttonAdd); // Ajout des boutons dans la cellule
 
-            const tdDelete = document.createElement("td"); // Création de la cellule pour le bouton Delete
+            // Création de la cellule pour le bouton Delete
+            const tdDelete = document.createElement("td");
             const buttonDelete = document.createElement("button"); // Création du bouton Delete
             buttonDelete.className = "btn btn-danger btn-sm product-del";
             buttonDelete.innerHTML = "&Cross;";
@@ -197,14 +158,66 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    // Création de la div contenant l'icone de suppression totale du stock
-    const divDeleteAll = document.getElementById("delete-all");
-    // Icone de suppression
+    // On récupère la thead du tableau
+    const headTableau = document.getElementById("th-table");
+    const tr = document.createElement('tr'); // On créé une ligne pour le tableau
+    tr.id = "tri";
+
+    // On créé les éléments de la ligne tri du tableau
+    // Tri par nom
+    const sortName = document.createElement("td");
+    sortName.innerHTML = "Tri par nom";
+    sortName.className = "tri";
+
+    // Tri par prix
+    const sortPrice = document.createElement("td");
+    sortPrice.innerHTML = "Tri par prix";
+    sortPrice.className = "tri";
+
+    // Tri par quantité
+    const sortQuantity = document.createElement("td");
+    sortQuantity.colSpan = 2;
+    sortQuantity.innerHTML = "Tri par quantité";
+    sortQuantity.className = "tri";
+
+    // Icone de suppression totale
+    const deleteAll = document.createElement("td");
     let deleteIcon = document.createElement("i");
     deleteIcon.className = "bi bi-trash icon delete-icon";
 
+    // On est à l'écoute du clic sur le nom pour trier par nom
+    sortName.addEventListener('click', () => {
+        let listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+        if (listeProduits.length > 1) { // On tri uniquement si on a plus d'un produit
+            localStorage.setItem('listeProduits', JSON.stringify(sortByName(listeProduits)));
+            listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+            displayProducts();
+        }
+    });
+
+    // On est à l'écoute du clic sur le prix pour trier par prix
+    sortPrice.addEventListener('click', () => {
+        let listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+        if (listeProduits.length > 1) { // On tri uniquement si on a plus d'un produit
+            localStorage.setItem('listeProduits', JSON.stringify(sortByPrice(listeProduits)));
+            listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+            displayProducts();
+        }
+    });
+
+    // On est à l'écoute du clic sur la quantité pour trier par quantité
+    sortQuantity.addEventListener('click', () => {
+        let listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+        if (listeProduits.length > 1) { // On tri uniquement si on a plus d'un produit
+            localStorage.setItem('listeProduits', JSON.stringify(sortByQuantity(listeProduits)));
+            listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
+            displayProducts();
+        }
+    });
+
     // Si l'utilisateur clique sur l'icone alors on efface tout
     deleteIcon.addEventListener("click", () => {
+        let listeProduits = JSON.parse(localStorage.getItem('listeProduits')) || [];
         if (window.confirm("Voulez-vous supprimer la totalité des produits du stock ?")) {
             //console.log("Suppression totale");
             listeProduits = [];
@@ -214,16 +227,18 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Ajout de l'icone dans le html
-    divDeleteAll.appendChild(deleteIcon);
+    deleteAll.appendChild(deleteIcon);
+
+    // On ajoute les éléments à la ligne
+    tr.append(sortName, sortPrice, sortQuantity, deleteAll);
+
+    headTableau.appendChild(tr); // On ajoute la ligne au tableau
 
     const formulaire = document.getElementById("ajoutForm");
 
     // Formulaire d'ajout d'un produit dans le stock
     formulaire.addEventListener('submit', (e) => {
         e.preventDefault(); // Empêche le fonctionnement normal du formulaire
-
-        let listeProduits = JSON.parse(localStorage.getItem("listeProduits")) || [];
 
         // On récupère les éléments du formulaire
         const inputName = document.getElementById("add-name");
